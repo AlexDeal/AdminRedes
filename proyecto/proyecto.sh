2bace7608f34202dd236513c5e60fd2cc7c4a5c8
alex <alejandro.serrano354@gmail.com>
2021-11-29 06:17:48 -0600

    Se corrigen las opciones 1-7

--- proyecto/proyecto.sh
+++ proyecto/proyecto.sh
@@ -1,79 +1,113 @@
 #!/bin/bash
 
 #Funciones
+
+#Paso número 1
 crearContraseña(){
-    #sudo -S sed -i 's/sha512/sha512 minline=12/g' /etc/pam.d/common-password
-    echo "Longitud modificada exitosamente"
-    read -p "Ingrese el nombre del usuario al que le quiere cambiar la contraseña: " nombre
-    ead -p "Ingrese la nueva contraseña: " pass
-    su $nombre
+    echo ""
+    if grep -q "sha512 minlen=12" /etc/pam.d/common-password; then
+        echo "Existe longitud minima y es de 12"
+    else
+        echo "No existe longitud minima"
+        sudo -S sed -i 's/sha512/sha512 minlen=12/g' /etc/pam.d/common-password
+        echo "Longitud modificada exitosamente"
+    fi
 }
 
+#Paso número 2
 crearUsuario(){
-    read -p "Ingrese el nombre del usuario" nombre
+    echo ""
+    read -p "Ingrese el nombre del usuario: " nombre
     sudo -S adduser $nombre
     getent passwd | grep $nombre
 }
 
+
+#Paso número 3
 crearArchivo(){
-    read -p "Ingrese el id del usuario" id
-    cat /var/log/syslog | grep $id >> /home/alex/Escritorio/acciones.txt
+    echo ""
+    read -p "Ingrese el id del usuario: " id
+    usuario=$(id -nu $id)
+    if id -u "$usuario" >/dev/null 2>&1; then
+        #sudo -s cat /var/log/syslog | grep $id >> /home/$usuario/Escritorio/acciones.txt
+        sudo -s cat /var/log/syslog | grep $id >> /home/kenshy/Escritorio/acciones.txt
+        echo "Operación completada con éxito"
+    fi
 }
 
+#Paso número 4
 accesosFallidos(){
-    read -p "Ingrese el usuario del usuario" usuario
-    faillog -u $usuario
+    echo ""
+    cat /var/log/auth.log | grep 'authentication failure'
+    #cat /var/log/auth.log | grep 'user NOT'
 }
 
+#Paso número 5
 accesoTecnicos(){
-    read -p "Ingrese el nombre del usuario" nombre
+    echo ""
+    read -p "Ingrese el nombre de usuario del técnico 1: " nombre
     sudo -S adduser $nombre
-    usermod -G admin $nombre
-    read -p "Ingrese el nombre del usuario" nombre
+    usermod -aG adm $nombre
+    usermod -aG Tecnicos $nombre
+    echo ""
+    read -p "Ingrese el nombre de usuario del técnico 2: " nombre
     sudo -S adduser $nombre
-    usermod -G admin $nombre
+    usermod -aG adm $nombre
+    usermod -aG Tecnicos $nombre
 }
 
+#Paso número 6
 deshabilitarServicio(){
+    echo ""
+    read -p "Ingrese el nombre del usuario: " GG
+    if grep '^Tecnicos:' /etc/group | grep -q $GG; then
+        sudo systemctl list-unit-files --type service --all
+        read -p "Ingrese el nombre del servicio que deseas detener: " servicio
+        sudo -S systemctl stop $servicio
+        sudo -S systemctl status $servicio | grep Active
+    else
+        echo "El usuario no tiene los permisos necesarios"
+    fi
     #Falta codigo para permitir ingresar a la sesion del tecnico
-    sudo systemctl list-unit-files --type service --all
-    read -p "Ingrese el nombre del servicio que deseas detener" servicio
-    sudo -S systemctl disable $servicio
 }
 
-clear
-echo "██████╗ ██████╗  ██████╗ ██╗   ██╗███████╗ ██████╗████████╗ ██████╗ "
-echo "██╔══██╗██╔══██╗██╔═══██╗╚██╗ ██╔╝██╔════╝██╔════╝╚══██╔══╝██╔═══██╗"
-echo "██████╔╝██████╔╝██║   ██║ ╚████╔╝ █████╗  ██║        ██║   ██║   ██║"
-echo "██╔═══╝ ██╔══██╗██║   ██║  ╚██╔╝  ██╔══╝  ██║        ██║   ██║   ██║"
-echo "██║     ██║  ██║╚██████╔╝   ██║   ███████╗╚██████╗   ██║   ╚██████╔╝"
-echo "╚═╝     ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝ ╚═════╝   ╚═╝    ╚═════╝ "
+#Paso número 7
+
+
                                                                     
-while [[ ! $opcion =~ ^[1-9] ]]; do
+while [[ ! $opcion =~ 8 ]]; do
+    clear
+    echo "██████╗ ██████╗  ██████╗ ██╗   ██╗███████╗ ██████╗████████╗ ██████╗ "
+    echo "██╔══██╗██╔══██╗██╔═══██╗╚██╗ ██╔╝██╔════╝██╔════╝╚══██╔══╝██╔═══██╗"
+    echo "██████╔╝██████╔╝██║   ██║ ╚████╔╝ █████╗  ██║        ██║   ██║   ██║"
+    echo "██╔═══╝ ██╔══██╗██║   ██║  ╚██╔╝  ██╔══╝  ██║        ██║   ██║   ██║"
+    echo "██║     ██║  ██║╚██████╔╝   ██║   ███████╗╚██████╗   ██║   ╚██████╔╝"
+    echo "╚═╝     ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝ ╚═════╝   ╚═╝    ╚═════╝ "
+    echo ""
     echo "1.- Establecer longitud minima de 12 caracteres a las contraseñas"
     echo "2.- Crear usuarios de acuerdo a las normas y visualizar informacion"
     echo "3.- Mandar un archivo con las ultimas acciones de un usuario"
     echo "4.- Ver todos los usuarios que han intentado entrar al servidor con contraseña erronea"
     echo "5.- Crear cuentas para tecnicos de soporte"
     echo "6.- Mostras los servicios activos del sistema y deshabilitar servicios"
-    echo "7.- DMostrar bitacoras"
-    read -p "Ingrese una opcion [1-7] " opcion
+    echo "7.- Mostrar bitacoras"
+    echo "8.- Salir"
+    echo ""
+    read -p "Ingrese una opcion [1-8]: " opcion
 
     case $opcion in
-        1)  crearContraseña
-            sleep 3;;
-        2)  crearUsuario
-            sleep 3;;
-        3)  crearArchivo
-            sleep 3;;
-        4)  accesosFallidos
-            sleep 3;;
-        5)  accesoTecnicos
-            sleep 3;;
-        6) deshabilitarServicio
-            sleep 3;;
+        1)  crearContraseña;;
+        2)  crearUsuario ;;
+        3)  crearArchivo ;;
+        4)  accesosFallidos ;;
+        5)  accesoTecnicos ;;
+        6) deshabilitarServicio ;;
         7) echo "Adios"
             exit 0;;
-        *) echo "Opcion no reconocida"
+        8) echo "Adios"
+            exit 0;;
+        *) echo "Opción no válida"
     esac
+    echo ""
+    read -p "Presione enter para continuar" enter
 done
